905a1153354e90d813e950b855032a49f77f3a1e
yuandongx <786018072@qq.com>
2020-08-19 08:24:55 +0000

    updae

--- web/src/layout/Footer.js
+++ web/src/layout/Footer.js
@@ -14,15 +14,11 @@ export default class extends React.Component {
       <Layout.Footer style={{padding: 0}}>
         <div className={styles.footerZone}>
           <div className={styles.linksZone}>
-            <a className={styles.links} title="官网" href="https://www.spug.dev"  target="_blank"
-               rel="noopener noreferrer">官网</a>
-            <a className={styles.links} title="Github" href="https://github.com/openspug/spug"  target="_blank"
+            <a className={styles.links} title="Github" href="https://github.com/yuandongx/iauto"  target="_blank"
                rel="noopener noreferrer"><Icon type="github" /></a>
-            <a title="文档" href="https://www.spug.dev/docs/about-spug/"  target="_blank"
-               rel="noopener noreferrer">文档</a>
           </div>
           <div style={{color: 'rgba(0, 0, 0, .45)'}}>
-            Copyright <Icon type="copyright"/> 2020 By OpenSpug
+            Copyright <Icon type="copyright"/> 2020 By Iauto
           </div>
         </div>
       </Layout.Footer>
