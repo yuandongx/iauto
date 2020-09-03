f920bea07c5e376785d4d69bd75fd3465e50dcb4
yuandongx <786018072@qq.com>
2020-09-03 14:57:13 +0800

    update

--- api/apps/exec/tasks.py
+++ api/apps/exec/tasks.py
@@ -3,36 +3,43 @@ import json
 from celery import shared_task
 from libs import Runner, human_datetime
 from apps.exec.models import Task, History
+import subprocess
+import re
 
 @shared_task
 def run_ansible(**kwargs):
     # results_callback = ResultsCollectorJSONCallback()
-    # kwargs['callback'] = results_callback
-    ansible = Runner(**kwargs)
-    ansible.run()
-    result = {}
-    # print("OK *******")
-    # for host, result in ansible.result_callback.host_ok.items():
-        # result.append('{0} >>> {1}'.format(host, result._result['stdout']))
-    # print("FAILED *******")
-    # for host, result in ansible.result_callback.host_failed.items():
-        # result.append('{0} >>> {1}'.format(host, result._result['msg']))
+    # # kwargs['callback'] = results_callback
+    # cmd = "ansible-playbook %s -i %s" % (kwargs['playbook'], kwargs['invntory'])
+    # print(cmd)
+    # p = subprocess.Popen(
+    #     cmd,
+    #     shell=True,
+    #     stdin=subprocess.PIPE,
+    #     stdout=subprocess.PIPE,
+    #     stderr=subprocess.PIPE,
+    #     text=True,
+    # )
+    # outs, errors = p.communicate()
+    #
+    # if errors != "" and not errors.strip().startswith("[WARNING]"):
+    #     return {"host": "testhost", "err": errors}
+    # else:
+    #     re_err = re.compile(r'fatal:\s+\[(.*)\]:\s+(\S*)\s+=>\s+(.*)')
+    #     result = re_err.findall(outs)
+    #     print(result)
+    playbooks = json.loads(kwargs.playbooks)
+    print(playbooks)
+    return kwargs
 
-    # print("DOWN *********")
-    # for host, result in ansible.result_callback.host_unreachable.items():
-        # result.append('{0} >>> {1}'.format(host, result._result['msg']))
-    # result['ok'] = ansible.result_callback.host_ok
-    # result['failed'] = ansible.result_callback.host_failed
-    # result['unreachable'] = ansible.result_callback.host_unreachable
-
-    history = History.objects.filter(pk='4345353').first()
-    if not history:
-        history = History.objects.create(
-            task_id=2,
-            status=2,
-            run_time=human_datetime(),
-            output=json.dumps(result)
-        )
-    else:
-        history.output=json.dumps(result)
-        history.save()
+    # history = History.objects.filter(pk='4345353').first()
+    # if not history:
+    #     history = History.objects.create(
+    #         task_id=2,
+    #         status=2,
+    #         run_time=human_datetime(),
+    #         output=json.dumps(result)
+    #     )
+    # else:
+    #     history.output=json.dumps(result)
+    #     history.save()
